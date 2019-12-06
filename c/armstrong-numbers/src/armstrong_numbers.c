50103b37b230b15debabb9eba0d3741c602c8132
Earl Lapus <lapus.er@sprobe.com>
2019-12-06 08:44:32 +0800

    initial submission

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,1 +1,20 @@
+#include <math.h>
+
 #include "armstrong_numbers.h"
+
+int is_armstrong_number(int candidate)
+{
+    int digits, q, r, res;
+
+    digits = 1;
+    for (q = candidate; (q /= 10) > 0; ++digits)
+        ; /* lonely loop - it has "nobody" ;) */
+
+    res = 0;
+    for (q = candidate; q > 0; q /= 10) {
+        r = q % 10;
+        res += pow(r, digits);
+    }
+
+    return (res == candidate);
+}
