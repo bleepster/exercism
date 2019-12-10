d84a4b2b7d151c30e6971678e10f2bc8058087e7
bleepster <earl.lapus@gmail.com>
2019-12-10 15:28:51 +0800

    put a message on exceptions

--- cpp/triangle/triangle.h
+++ cpp/triangle/triangle.h
@@ -14,7 +14,7 @@ bool isValid(T a, T b, T c) {
 
 template <typename T>
 flavor kind(T a, T b, T c) {
-  if (!isValid(a, b, c)) throw std::domain_error("");
+  if (!isValid(a, b, c)) throw std::domain_error("not a triangle");
   if (a == b && b == c)
     return flavor::equilateral;
   else if ((a == b && b != c) || (b == c && c != a) || (c == a && b != c))
@@ -22,7 +22,9 @@ flavor kind(T a, T b, T c) {
   else if (a != b && b != c && a != c)
     return flavor::scalene;
   else
-    throw std::domain_error("");
+    // throw an error if all conditions above are not met
+    // such condition may not exist, just being explicit
+    throw std::domain_error("unknown kind");
 }
 }  // namespace triangle
 
