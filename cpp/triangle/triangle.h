101e85921f41012dd7bb1aab7b94a6ea6fdd3b56
bleepster <earl.lapus@gmail.com>
2019-12-10 10:52:56 +0800

    add cpp track, triangle - initial submission

---
+++ cpp/triangle/triangle.h
@@ -0,0 +1,33 @@
+#if !defined(TRIANGLE_H)
+#define TRIANGLE_H
+
+#include <stdexcept>
+
+namespace triangle {
+namespace flavor {
+constexpr int equilateral = 0;
+constexpr int isosceles = 1;
+constexpr int scalene = 2;
+}
+
+template <typename T>
+bool isValid(T a, T b, T c) {
+  return ((a > 0 && b > 0 && c > 0) &&
+          (a + b >= c && b + c >= a && c + a >= b));
+}
+
+template <typename T>
+int kind(T a, T b, T c) {
+  if (!isValid(a, b, c)) throw std::domain_error("");
+  if (a == b && b == c)
+    return flavor::equilateral;
+  else if ((a == b && b != c) || (b == c && c != a) || (c == a && b != c))
+    return flavor::isosceles;
+  else if (a != b && b != c && a != c)
+    return flavor::scalene;
+  else
+    throw std::domain_error("");
+}
+}  // namespace triangle
+
+#endif  // TRIANGLE_H
