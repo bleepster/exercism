c1ee2af9390009efd51df42d0f73dbdf317de06d
bleepster <earl.lapus@gmail.com>
2019-12-11 17:06:12 +0800

    initial implementation - some tests failing

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,22 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+fn make_comparable(w: &str) -> String {
+    let mut t: Vec<u8> = w.bytes().map(|b: u8| b.to_ascii_lowercase()).collect();
+    t.sort_by(|a, b| (*a as char).cmp(&(*b as char)));
+    String::from_utf8(t).unwrap()
+}
+
+pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let _word: String = make_comparable(word);
+    let anagrams = possible_anagrams
+        .iter()
+        .filter_map(|a| -> Option<&str> {
+            let _a = make_comparable(a);
+            match _word == _a {
+                true => Some(a),
+                _ => None,
+            }
+        })
+        .collect::<Vec<&str>>();
+    HashSet::from(anagrams.iter().cloned().collect())
 }
