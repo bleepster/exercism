9fd2c8143f14d596fd47bf80ff9522649e24e793
bleepster <earl.lapus@gmail.com>
2019-12-12 11:39:11 +0800

    another set of updates to make tests pass

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,18 +1,18 @@
 use std::collections::HashSet;
 
 fn make_comparable(w: &str) -> String {
-    let mut t: Vec<u8> = w.bytes().map(|b: u8| b.to_lowercase()).collect();
+    let mut t: Vec<u8> = w.bytes().collect();
     t.sort_by(|a, b| a.cmp(b));
-    String::from_utf8(t).unwrap()
+    String::from_utf8_lossy(&t).to_string()
 }
 
 pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let _word: String = make_comparable(word);
+    let _word: String = make_comparable(&word.to_lowercase());
     let anagrams = possible_anagrams
         .iter()
         .filter_map(|a| -> Option<&str> {
-            let _a = make_comparable(a);
-            match (a != &word, _word == _a) {
+            let _a = make_comparable(&a.to_lowercase());
+            match (a.to_lowercase() != word.to_lowercase(), _word == _a) {
                 (true, true) => Some(a),
                 _ => None,
             }
