1fc09fcd4d320a692922058aba12ed523d6ac71e
bleepster <earl.lapus@gmail.com>
2019-12-17 07:59:52 +0800

    review suggestion: use enum

--- c/resistor-color/src/resistor_color.h
+++ c/resistor-color/src/resistor_color.h
@@ -1,18 +1,9 @@
 #ifndef RESISTOR_COLOR_H
 #define RESISTOR_COLOR_H
 
-#define BLACK 0
-#define BROWN 1
-#define RED 2
-#define ORANGE 3
-#define YELLOW 4
-#define GREEN 5
-#define BLUE 6
-#define VIOLET 7
-#define GREY 8
-#define WHITE 9
-
-typedef int resistor_band_t;
+typedef enum {
+    BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE
+} resistor_band_t;
 
 resistor_band_t* colors();
 int color_code(int color);
