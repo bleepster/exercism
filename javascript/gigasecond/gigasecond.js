8ca06272b79ce1e679cf7ce5ded01eae843487bc
bleepster <earl.lapus@gmail.com>
2019-12-18 10:02:56 +0800

    initial submission

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,6 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error("Remove this statement and implement this function");
+export const gigasecond = (date) => {
+    return new Date(date.getTime() + (1000000000 * 1000));
 };
