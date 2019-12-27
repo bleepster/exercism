5e7f70ace6042a1578babf50be9c8eda9c8da1cc
bleepster <earl.lapus@gmail.com>
2019-12-27 11:00:03 +0800

    improvements to existing implementation
    - make create_char_map() more concise
    - short-circuit anagram matching by "subtracting" values

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -2,28 +2,11 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 
 fn create_char_map(s: &str) -> HashMap<char, u32> {
-    let mut m = HashMap::new();
+    let mut m = HashMap::with_capacity(s.len());
     for c in s.chars() {
-        if let Some(x) = m.get_mut(&c) {
-            *x = *x + 1;
-        } else {
-            m.insert(c, 1);
-        }
+        m.insert(c, m.get(&c).or_else(|| Some(&0)).unwrap() + 1);
     }
     m
-}
-
-fn is_match(word: &HashMap<char, u32>, maybe_anagram: &HashMap<char, u32>) -> bool {
-    for (k, v) in word.iter() {
-        if let Some(x) = maybe_anagram.get(&k) {
-            if v != x {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-    true
 }
 
 pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
@@ -33,15 +16,26 @@ pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&'a str]) -> HashSet
         .iter()
         .filter_map(|a| -> Option<&str> {
             let a_lowered = a.to_lowercase();
-            let a_mapped = create_char_map(&a_lowered);
-            match (
-                word.len() == a.len(),
-                w_lowered != a_lowered,
-                is_match(&w_mapped, &a_mapped),
-            ) {
-                (true, true, true) => Some(a),
-                _ => None,
+            if w_lowered == a_lowered {
+                return None;
             }
+
+            let mut w = w_mapped.clone();
+            for c in a_lowered.chars() {
+                if let Some(x) = w.get_mut(&c) {
+                    *x = *x - 1;
+                    if *x == 0 {
+                        w.remove(&c);
+                    }
+                } else {
+                    return None;
+                }
+            }
+
+            if !w.is_empty() {
+                return None;
+            }
+            Some(a)
         })
         .collect::<Vec<&str>>();
     HashSet::from(anagrams.iter().cloned().collect())
