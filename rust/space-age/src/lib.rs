14201e076e6c79b4f19b12061c767210a8209dbb
bleepster <earl.lapus@gmail.com>
2020-01-10 09:19:22 +0800

    changes based on review
    - use contants
    - define only one years_during() implmentation

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -3,19 +3,23 @@
 
 #[derive(Debug)]
 pub struct Duration {
-    value: f64,
+    value: u64,
 }
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        Duration {
-            value: s as f64 / 31557600 as f64,
-        }
+        Self { value: s }
     }
 }
 
 pub trait Planet {
-    fn years_during(d: &Duration) -> f64;
+    const EA_SECONDS: f64 = 31557600.0;
+    const EARTH_YEARS: f64;
+
+    fn years_during(d: &Duration) -> f64 {
+        let duration = d.value as f64;
+        (duration / Self::EA_SECONDS) / Self::EARTH_YEARS
+    }
 }
 
 pub struct Mercury;
@@ -28,49 +32,33 @@ pub struct Uranus;
 pub struct Neptune;
 
 impl Planet for Mercury {
-    fn years_during(d: &Duration) -> f64 {
-        d.value / 0.2408467
-    }
+    const EARTH_YEARS: f64 = 0.2408467;
 }
 
 impl Planet for Venus {
-    fn years_during(d: &Duration) -> f64 {
-        d.value / 0.61519726
-    }
+    const EARTH_YEARS: f64 = 0.61519726;
 }
 
 impl Planet for Earth {
-    fn years_during(d: &Duration) -> f64 {
-        d.value / 1.0
-    }
+    const EARTH_YEARS: f64 = 1.0;
 }
 
 impl Planet for Mars {
-    fn years_during(d: &Duration) -> f64 {
-        d.value / 1.8808158
-    }
+    const EARTH_YEARS: f64 = 1.8808158;
 }
 
 impl Planet for Jupiter {
-    fn years_during(d: &Duration) -> f64 {
-        d.value / 11.862615
-    }
+    const EARTH_YEARS: f64 = 11.862615;
 }
 
 impl Planet for Saturn {
-    fn years_during(d: &Duration) -> f64 {
-        d.value / 29.447498
-    }
+    const EARTH_YEARS: f64 = 29.447498;
 }
 
 impl Planet for Uranus {
-    fn years_during(d: &Duration) -> f64 {
-        d.value / 84.016846
-    }
+    const EARTH_YEARS: f64 = 84.016846;
 }
 
 impl Planet for Neptune {
-    fn years_during(d: &Duration) -> f64 {
-        d.value / 164.79132
-    }
+    const EARTH_YEARS: f64 = 164.79132;
 }
