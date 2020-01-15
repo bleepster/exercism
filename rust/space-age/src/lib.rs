862b0d01d9dc08888ca0cc23646c2d31bb87423a
bleepster <earl.lapus@gmail.com>
2020-01-15 20:26:54 +0800

    create a macro for planet implementation

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -22,43 +22,24 @@ pub trait Planet {
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
-
-impl Planet for Mercury {
-    const EARTH_YEARS: f64 = 0.2408467;
+macro_rules! planet {
+    { $( $p: ident, $e: expr );* } => {
+        $(
+            pub struct $p;
+            impl Planet for $p {
+                const EARTH_YEARS: f64 = $e;
+            }
+        )*
+    }
 }
 
-impl Planet for Venus {
-    const EARTH_YEARS: f64 = 0.61519726;
-}
-
-impl Planet for Earth {
-    const EARTH_YEARS: f64 = 1.0;
-}
-
-impl Planet for Mars {
-    const EARTH_YEARS: f64 = 1.8808158;
-}
-
-impl Planet for Jupiter {
-    const EARTH_YEARS: f64 = 11.862615;
-}
-
-impl Planet for Saturn {
-    const EARTH_YEARS: f64 = 29.447498;
-}
-
-impl Planet for Uranus {
-    const EARTH_YEARS: f64 = 84.016846;
-}
-
-impl Planet for Neptune {
-    const EARTH_YEARS: f64 = 164.79132;
+planet! {
+    Mercury, 0.240847;
+    Venus, 0.61519726;
+    Earth, 1.0;
+    Mars, 1.8808158;
+    Jupiter, 11.862615;
+    Saturn, 29.44749;
+    Uranus, 84.016846;
+    Neptune, 164.79132
 }
